3b739fa9d8fbd8fa4897a2765ce2a28d70937ce4
Nathan Long <nrlong13@gmail.com>
2021-02-01 14:58:47 -0500

    updated js file to reflect image changes for dice rolls on refresh

--- script/script.js
+++ script/script.js
@@ -4,3 +4,11 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document.querySelector("img.img1").setAttribute("src", "images/dice" + randomNumber1 +".png");
 
 document.querySelector("img.img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if(randomNumber1 < randomNumber2){
+    document.getElementsByTagName("h1")[0].innerHTML = "🚩 Player 1 Wins!!!"
+}else if (randomNumber2 < randomNumber1){
+    document.getElementsByTagName("h1")[0].innerHTML = "Player 2 Wins!!! 🚩"
+}else{
+    document.getElementsByTagName("h1")[0].innerHTML = "🚩 ITS A DRAW!! 🚩"
+}
